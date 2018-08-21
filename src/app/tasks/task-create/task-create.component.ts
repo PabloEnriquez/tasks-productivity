1606c43c5a7c5d40f96c19968822636e99975882
PabloEnriquez <12pable34@gmail.com>
2018-08-21 01:07:12 -0500

    First component

---
+++ src/app/tasks/task-create/task-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-create',
+  templateUrl: './task-create.component.html'
+})
+export class TaskCreateComponent {
+
+}
